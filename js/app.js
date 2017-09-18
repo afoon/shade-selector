24f8b280994469cc1bcae22a72c711f4f7e0b357
Aminata Foon <aminatafoon@Aminatas-MacBook-Air.local>
2017-09-18 09:06:36 -0500

    git pages

--- js/app.js
+++ js/app.js
@@ -3,22 +3,22 @@ angular.module('shadeApp',['ui.router']).config(function ($stateProvider, $urlRo
         $stateProvider
             .state('home', {
                 url: '/',
-                templateUrl: './views/home.html',
+                templateUrl: '/views/home.html',
                 // controller: 'mainCtrl'
             })
             .state('shades', {
                 url: '/shades',
-                templateUrl: './views/shades.html',
+                templateUrl: '/views/shades.html',
                 // controller: 'mainCtrl'
             })
             .state('about', {
                 url: '/about',
-                templateUrl: './views/about.html',
+                templateUrl: '/views/about.html',
                 // controller: 'mainCtrl'
             })
             .state('results', {
                 url: '/results/:type',
-                templateUrl: './views/results.html',
+                templateUrl: '/views/results.html',
                 controller: 'mainCtrl'
             });
     
