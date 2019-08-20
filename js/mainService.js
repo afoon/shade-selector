480824512013edeeb8b824847be71b7ace006a74
Aminata Foon <am.e.foon@gmail.com>
2019-08-19 20:40:39 -0400

    Update mainService.js

--- js/mainService.js
+++ js/mainService.js
@@ -2,7 +2,7 @@ angular.module('shadeApp').service('mainService',function($http){
     this.getMakeup = function () {
         var promise = $http({
             method:'GET', 
-            url:'http://makeup-api.herokuapp.com/api/v1/products.json?product_type=foundation'
+            url:'https://makeup-api.herokuapp.com/api/v1/products.json?product_type=foundation'
         });
 return promise;
 console.log(promise);
